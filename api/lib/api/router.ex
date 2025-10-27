cd65c1319148fe7461ff0d8fed3304e5536e4708
at-github-th <at.github.th@gmail.com>
2025-10-27 17:02:21 +0000

    fix: enable CORS + correct ports + working example routes

--- api/lib/api/router.ex
+++ api/lib/api/router.ex
@@ -1,16 +1,14 @@
-
 defmodule Api.Router do
   use Plug.Router
   plug Plug.Logger
+  plug :cors
   plug :match
   plug Plug.Parsers, parsers: [:json], json_decoder: Jason
   plug :dispatch
-
-  get "/" do
-    send_resp(conn, 200, ~s({"name":"Geospatial Venue Map + Heatmaps API","ok":true}))
-  end
-
-  get "/api/venues" do
-    send_resp(conn, 200, ~s([{"id":"V-1","name":"HQ"},{"id":"V-2","name":"Park West"}]))
-  end
+  defp cors(conn, _), do:
+    conn |> Plug.Conn.put_resp_header("access-control-allow-origin","*")
+        |> Plug.Conn.put_resp_header("access-control-allow-headers","*")
+        |> Plug.Conn.put_resp_header("access-control-allow-methods","*")
+  get "/" do send_resp(conn, 200, ~s({"name":"Geospatial Venue Map API","ok":true})) end
+  get "/api/venues" do send_resp(conn, 200, ~s([{"id":"V-1","name":"HQ"},{"id":"V-2","name":"Park West"}])) end
 end
